9ecd2f5688235b0dbfb156be8748c2fbf11c15f3
zeng <752506363@qq.com>
2018-08-23 18:04:27 +0800

    配置公共component_build

--- module-gank/src/main/java/com/zeng/art/module_gank/GankActivity.java
+++ module-gank/src/main/java/com/zeng/art/module_gank/GankActivity.java
@@ -1,6 +1,7 @@
 package com.zeng.art.module_gank;
 
 import android.os.Bundle;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
