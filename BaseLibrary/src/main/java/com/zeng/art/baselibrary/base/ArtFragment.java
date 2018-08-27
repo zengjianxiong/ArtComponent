1fce740651da94bf2a3e865caf10005667fca088
zeng <752506363@qq.com>
2018-08-27 13:01:39 +0800

    添加工具类

--- BaseLibrary/src/main/java/com/zeng/art/baselibrary/base/ArtFragment.java
+++ BaseLibrary/src/main/java/com/zeng/art/baselibrary/base/ArtFragment.java
@@ -13,9 +13,40 @@ import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import dagger.android.support.AndroidSupportInjection;
 
-public abstract class ArtFragment<P extends ArtPresenter<V>, V extends ArtView> extends TiFragment<P,V> {
+public abstract class ArtFragment<P extends ArtPresenter<V>, V extends ArtView> extends TiFragment<P, V> {
     private Unbinder mUnBinder;
     public View mRootView;
+    private boolean isFragmentVisible;
+
+    private boolean isFirstVisible;
+
+
+    //setUserVisibleHint()在Fragment创建时会先被调用一次，传入isVisibleToUser = false
+    //如果当前Fragment可见，那么setUserVisibleHint()会再次被调用一次，传入isVisibleToUser = true
+    //如果Fragment从可见->不可见，那么setUserVisibleHint()也会被调用，传入isVisibleToUser = false
+    //总结：setUserVisibleHint()除了Fragment的可见状态发生变化时会被回调外，在new Fragment()时也会被回调
+
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (mRootView == null) {
+            return;
+        }
+        if (isFirstVisible && isVisibleToUser) {
+            onFragmentFirstVisible();
+            isFirstVisible = false;
+        }
+        if (isVisibleToUser) {
+            onFragmentVisibleChange(true);
+            isFragmentVisible = true;
+            return;
+        }
+        if (isFragmentVisible) {
+            isFragmentVisible = false;
+            onFragmentVisibleChange(false);
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,18 +56,31 @@ public abstract class ArtFragment<P extends ArtPresenter<V>, V extends ArtView> 
     }
 
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        mRootView = inflater.inflate(getLayoutId(), container, false);
         super.onCreateView(inflater, container, savedInstanceState);
-        return mRootView;
-
+        return inflater.inflate(getLayoutId(), container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        //如果setUserVisibleHint()在rootView创建前调用时，那么
+        //就等到rootView创建完后才回调onFragmentVisibleChange(true)
+        //保证onFragmentVisibleChange()的回调发生在rootView创建完成之后，以便支持ui操作
+        if (mRootView == null) {
+            mRootView = view;
+            if (getUserVisibleHint()) {
+                if (isFirstVisible) {
+                    onFragmentFirstVisible();
+                    isFirstVisible = false;
+                }
+                onFragmentVisibleChange(true);
+                isFragmentVisible = true;
+            }
+        }
+
         setUnBinder(ButterKnife.bind(this, view));
         setUp();
         super.onViewCreated(view, savedInstanceState);
@@ -61,5 +105,38 @@ public abstract class ArtFragment<P extends ArtPresenter<V>, V extends ArtView> 
             mUnBinder.unbind();
         }
         super.onDestroy();
+        initVariable();
+    }
+
+    private void initVariable() {
+        isFirstVisible = true;
+        isFragmentVisible = false;
+        mRootView = null;
+
+    }
+
+
+    /**
+     * 去除setUserVisibleHint()多余的回调场景，保证只有当fragment可见状态发生变化时才回调
+     * 回调时机在view创建完后，所以支持ui操作，解决在setUserVisibleHint()里进行ui操作有可能报null异常的问题
+     * <p>
+     * 可在该回调方法里进行一些ui显示与隐藏
+     *
+     * @param isVisible true  不可见 -> 可见
+     *                  false 可见  -> 不可见
+     */
+    protected void onFragmentVisibleChange(boolean isVisible) {
+
+    }
+
+    /**
+     * 在fragment首次可见时回调，可用于加载数据，防止每次进入都重复加载数据
+     */
+    protected void onFragmentFirstVisible() {
+
+    }
+
+    protected boolean isFragmentVisible() {
+        return isFragmentVisible;
     }
 }
